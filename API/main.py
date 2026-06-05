add15a7c82fa44c391553365f8671d3fbf51cc9b
Ricky Hou <richardhouston18@gmail.com>
2026-06-05 16:43:30 -0400

    allow credentials

--- API/main.py
+++ API/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "https://fullstackpractice-api.onrender.com"],
-    
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
