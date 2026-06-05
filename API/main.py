2fe7ca04e7d1212ca414f604e4259e6735e2b358
Ricky Hou <richardhouston18@gmail.com>
2026-06-05 17:06:28 -0400

    add production URL to cors

--- API/main.py
+++ API/main.py
@@ -9,7 +9,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://fullstackpractice-8h8hqjbha-hourickys-projects.vercel.app",],
+    "https://fullstackpractice-8h8hqjbha-hourickys-projects.vercel.app", 
+    "https://fullstackpractice-iota.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
