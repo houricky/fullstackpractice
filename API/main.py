bc3c3bfa66a495154dd3bf2f07c8986e46252f18
Ricky Hou <richardhouston18@gmail.com>
2026-06-05 16:36:45 -0400

    added backend URL

--- API/main.py
+++ API/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://bank-console-app.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://fullstackpractice-api.onrender.com"],
     
     allow_methods=["*"],
     allow_headers=["*"],
